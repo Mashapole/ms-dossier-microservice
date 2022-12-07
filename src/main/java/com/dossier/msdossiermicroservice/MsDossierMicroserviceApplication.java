5b0bd5353064bf19c39dee8b55ab5da21fa8e36a
Mashapole <ralezemoshake@gmail.com>
2022-12-07 19:33:11 +0200

    mvp-4

---
+++ src/main/java/com/dossier/msdossiermicroservice/MsDossierMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.dossier.msdossiermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDossierMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDossierMicroserviceApplication.class, args);
+	}
+
+}
